d0601bd7d68982633aec77a2c8dc970218d43424
krupaliKore <156192609+krupaliKore@users.noreply.github.com>
2024-01-10 18:08:59 +0530

    add cpp

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ using namespace std;
+ int main()
+ {
+ 	int num1,num2 ,Result;
+ 	cout<<"Enter two Number: "<<endl;
+ 	    cin>>num1;
+ 		cin>>num2;
+ 	Result = num1+num2;
+ 
+ 	cout<<"Addition :"<<Result<<endl;
+ 	
+ 	return 0;
+ }
+
+
